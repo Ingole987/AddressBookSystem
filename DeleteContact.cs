48e1cc25a3a8b7c3b8c1527cd9af0abf7e617918
Ingole987 <pratik.ingole98@gmail.com>
2022-01-23 14:31:15 +0530

    added:DeleteContact

---
+++ DeleteContact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class DeleteContact
+    {
+        public void DeleteContact()
+        {
+            Console.WriteLine("Enter First Name of the Contact you want to Delete:");
+            string name = Console.ReadLine();
+            foreach (var contact in List)
+            {
+                if (contact.FirstName == name)
+                {
+                    List.Remove(contact);
+                    Console.WriteLine("---------------------------------\n!!Contact Removed!!");
+                    break;
+                }
+            }
+        }
+
+    }
+}
