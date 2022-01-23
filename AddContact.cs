dc7f8d938bba3e99a4f7901f54c938e0e3af9257
Ingole987 <pratik.ingole98@gmail.com>
2022-01-23 14:24:18 +0530

    added:addcontact

---
+++ AddContact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddContact
+    {
+        public void StoreContact()
+        {
+            Console.WriteLine("Enter a unique name to your Addressbook:");
+            string addressbookName = Console.ReadLine();
+            Console.WriteLine("First Name:");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Zip Code:");
+            string zipcode = Console.ReadLine();
+            Console.WriteLine("Phone Number:");
+            string phonenumber = Console.ReadLine();
+            Console.WriteLine("Email:");
+            string email = Console.ReadLine();
+            var newContact = new Contact(firstname, lastname, address, city, state, zipcode, phonenumber, email);
+            DictName.Add(addressbookName, newContact);
+
+            public void DisplayAllContact()
+            {
+                foreach (var element in DictName)
+                {
+                    Console.WriteLine("----------------\nADDRESSBOOK :\n----------------\nAddressbook Key: " + element.Key + "\nPerson's Name: " + element.Value.FirstName + " " + element.Value.LastName + "\nAddress: " + element.Value.Address + "\nCity: " + element.Value.City + "\nState: " + element.Value.State + "\nZip Code: " + element.Value.ZipCode + "\nPhone Number: " + element.Value.PhoneNumber + "\nEmail: " + element.Value.Email);
+                }
+            }
+        }
+    }
+}
