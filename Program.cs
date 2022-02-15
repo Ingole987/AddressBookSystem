166a6d1550dfa7c5ac3a9126ccf8af77510bebd5
Ingole987 <pratik.ingole98@gmail.com>
2022-02-15 18:18:57 +0530

    feat : updated program

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddressBookSystem;
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to Address Book.");
+            Console.WriteLine("=================================\nChoose the operation:\n1.Add Contact.\n2.View Saved Contacts.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n=================================");
+            var userInput = Console.ReadLine();
+            var addressBook = new AddressBook();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        addressBook.StoreContact();
+                        break;
+                    case "2":
+                        addressBook.DisplayAllContact();
+                        break;
+                    case "3":
+                        addressBook.EditContact();
+                        break;
+                    case "4":
+                        addressBook.DeleteContact();
+                        break;
+                    default:
+                        Console.WriteLine("!!! Choose valid operation !!!");
+                        break;
+                }
+                Console.WriteLine("=================================\nChoose the operation:\n1.Add Another Contact.\n2.View Saved Contacts.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n=================================");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
